e3fa7d1fd8940af14a66a71b959da6c603d59205
Sophie <sophie88leigh@gmail.com>
2019-10-31 22:23:27 -0400

    commiting initial stage of prototype. created input box and add button

---
+++ .history/script_20191031202533.js
@@ -0,0 +1,13 @@
+
+    <div id="left-box">
+    <form id="weather-form">
+        <label for="weather-input">Search for weather:</label>
+        <br>
+        <input type="text" id="movie-input" placeholder="Zipcode">
+
+        <!-- Button will trigger new zipcode to be added -->
+        <input id="add " class="btn btn-primary mb-2" type="submit" value="Add a zipcode">
+    </form>
+
+    <div id="zipcode-view"></div>
+</div>
