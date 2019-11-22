adb09e89784dbc0b386048b22ece8d5590bedc77
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-11-21 21:01:34 -0500

    major refactoring of js organization, created new functions where applicable to clean up search button submit event

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//init chips function from materializeCSS
+$("#ingredients").chips();
+
+//div grabbers
 const form = $("#form-column");
 const input = $("#ingredients-input");
 const chipsColumn = $("#chips-column");
 const clearItemsBtn = $("#clear-items-btn")
 const mainResultsContainer = $("#main-results-container");
 const recipeView = $("#recipe-view");
+
+//init ingredients array for chips data to input into ajax call 
 let ingredientsArray = [];
 
 //localStorage 
@@ -13,102 +19,62 @@ if (storedSearch !== null) {
     searchedArrays = JSON.parse(storedSearch)
 } console.log(searchedArrays);
 
-// clear all items btn event
-clearItemsBtn.on("click", function () {
-    window.location.reload(true);
-});
-
-// button to submit chip array for results
+//button to submit chip array for results
 form.on("submit", function (e) {
+    //prevent form submit default
     e.preventDefault();
-
     //clear recipe results container
     recipeView.empty();
-
     //compile chips into array function
     getChipsArray();
-
+    //set local storage from chips array
     localStorage.setItem("searchedArrays", JSON.stringify(searchedArrays));
 
-    //Spoonacular AJAX Data function & arguments
-    getSpoonacularData(ingredientsArray.join(","), 50, function (response) {
-        // console.log(response)
+    //AJAX Call to Spoonacular including recipe print functionality
+    getSpoonacularData(ingredientsArray.join(","), 5, function (response) {
 
-        // generate results container
-        mainResultsContainer.removeClass("displayNone");
-        clearItemsBtn.removeClass("displayNone");
-        resultH3 = $("#result-h3");
-        resultH3.text("Your Recipes:");
+        generateResultsContainer();
 
-        // scroll to results on click
-        $('html, body').animate({
-            scrollTop: $("#main-results-container").offset().top
-        }, 1000);
+        scrollToResults();
 
-        // for each loop that gets results from array
+        //loop that prints recipe results from response object
         response.forEach(function (recipe) {
-            var pOne = $("<p id='recipeHeader'>").text(recipe.title).attr("data-id", recipe.id)
-            var base = "https://spoonacular.com/recipes/";
-            var title = encodeURI(recipe.title.replace("%20", "-"))
 
-            //remove recipe array upon re-pressing search
-            var resultsArray = $('recipe-view');
-            // while (resultsArray.firstChild) {
-            //     resultsArray.removeChild(resultsArray.firstChild)
-            // };
-            resultsArray.empty();
+            refreshResultsArray();
 
             //create dynamic url for recipe link
+            var base = "https://spoonacular.com/recipes/";
             var url = `${base}${title}-${recipe.id}`;
+
+            //create content for printed results
+            var pOne = $("<p id='recipeHeader'>").text(recipe.title).attr("data-id", recipe.id);
+            var title = encodeURI(recipe.title.replace("%20", "-"));
 
             //turn image into link
             var link = $("<a target = '_blank'>").attr("href", url)
-
-            //create img
             var img = $("<img>");
-
             //set image sttributes for source from api
-            img.attr({
-                src: recipe.image,
-            });
-
+            img.attr({ src: recipe.image, });
             //add image width class to contain inside container
             img.addClass("image-width");
-
             //apend img variable to link variable
             link.append(img);
 
+            //create card div
             var resultsCardDiv = $("<div class='card'>");
-
-
+            //print content to results card
             resultsCardDiv.append(pOne, link);
-
-            // prepare to append final results to page
+            //prepare to append final results to page
             recipeView.prepend(resultsCardDiv);
-        })
+        });
 
-        // append final results to page
-        // recipeView.append(recipeDiv);
+    });
 
-
-    })
-
-    // scrollToResults();
-})
-
-//scroll to top button
-var scrollTop = $("#scrollTop");
-
-scrollTop.on("click", function () {
-    $('html, body').scrollTop(0);
 });
 
-// init chips function, materialize
-$("#ingredients").chips();
+// *********FUNCTIONS**********
 
-
-
-// createunction to return results from API
+//return results from API
 function getSpoonacularData(searchItem, number, callBack) {
     $.ajax({
         "async": true,
@@ -146,18 +112,38 @@ function getChipsArray() {
     searchedArrays.push(ingredientsArray);
     console.log(searchedArrays);
 
-}
+};
+
+function generateResultsContainer() {
+
+    mainResultsContainer.removeClass("displayNone");
+    clearItemsBtn.removeClass("displayNone");
+    resultH3 = $("#result-h3");
+    resultH3.text("Your Recipes:");
+};
+
+function scrollToResults() {
+    $('html, body').animate({
+        scrollTop: $("#main-results-container").offset().top
+    }, 1000);
+};
+
+function refreshResultsArray() {
+    var resultsArray = $('recipe-view');
+    resultsArray.empty();
+};
 
 
+// *********BUTTONS**********
 
+//scroll to top button
+var scrollTop = $("#scrollTop");
+scrollTop.on("click", function () { $('html, body').scrollTop(0); });
 
-
-
-
-
-
-
-
+//clear all items btn event
+clearItemsBtn.on("click", function () {
+    window.location.reload(true);
+});
 
 
 
